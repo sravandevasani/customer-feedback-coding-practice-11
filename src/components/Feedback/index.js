ab7bfd0909fbd97c0a29a5854afea184f977f830
shravandeva <shravandevasni@gmail.com>
2023-03-15 11:07:42 +0000

    seperated and moved into methods

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -9,48 +9,56 @@ class Feedback extends Component {
     this.setState(prevState => ({isClicked: !prevState.isClicked}))
   }
 
-  render() {
+  renderThankyouScreen = () => {
     const {resources} = this.props
 
-    const {emojis, loveEmojiUrl} = resources
+    const {loveEmojiUrl} = resources
 
+    return (
+      <div className="content-container">
+        <img className="love-image" alt="love emoji" src={loveEmojiUrl} />
+        <h1 className="thanku-heading">Thank you</h1>
+        <p className="description">
+          We will use your feedback to improve our customer support performance
+        </p>
+      </div>
+    )
+  }
+
+  renderFeedbackScreen = () => {
+    const {resources} = this.props
+
+    const {emojis} = resources
+
+    return (
+      <div className="content-container">
+        <h1 className="heading">
+          How satisfied are you with our customer support performance
+        </h1>
+        <ul className="emojis-container">
+          {emojis.map(eachObj => (
+            <li key={eachObj.id}>
+              <img
+                className="emoji-icon"
+                alt={eachObj.name}
+                src={eachObj.imageUrl}
+                onClick={this.onClickDisplayThankYou}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
     const {isClicked} = this.state
 
-    console.log(emojis)
-    console.log(loveEmojiUrl)
-    console.log(isClicked)
     return (
       <div className="bg-container">
         <div className="content-bg-container">
-          {isClicked && (
-            <div className="content-container">
-              <img className="love-image" alt="love emoji" src={loveEmojiUrl} />
-              <h1 className="thanku-heading">Thank you</h1>
-              <p className="description">
-                We will use your feedback to improve our customer support
-                performance
-              </p>
-            </div>
-          )}
-          {!isClicked && (
-            <div className="content-container">
-              <h1 className="heading">
-                How satisfied are you with our customer support performance
-              </h1>
-              <ul className="emojis-container">
-                {emojis.map(eachObj => (
-                  <li key={eachObj.id}>
-                    <img
-                      className="emoji-icon"
-                      alt={eachObj.name}
-                      src={eachObj.imageUrl}
-                      onClick={this.onClickDisplayThankYou}
-                    />
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+          {isClicked && this.renderThankyouScreen()}
+          {!isClicked && this.renderFeedbackScreen()}
         </div>
       </div>
     )
